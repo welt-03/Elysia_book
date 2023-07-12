53eeaec0733d6ab89e0bb42fd314f5ba042a80b5
welt-03 <1557850953@qq.com>
2023-07-12 16:29:16 +0800

    为audio库添加了成员函数

--- components/audio/audio.cpp
+++ components/audio/audio.cpp
@@ -1,5 +1,15 @@
 #include "audio.h"
 
+uint8_t calculateADD8Check(const uint8_t *data, int length)
+{
+    uint8_t checksum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        checksum += data[i];
+    }
+    return checksum;
+}
+
 Audio::Audio()
 {
     _port = UART_NUM_1;
@@ -14,7 +24,112 @@ Audio::~Audio()
 {
 }
 
-void Audio::write(const char *data)
+void Audio::init()
 {
-    uart_write_bytes(_port, data, strlen(data));
+    _volume = 30;
+    this->write(toSDcard_cmd);
+    this->write(volumeSet_cmd);
+    this->write(singlePlay_cmd);
+}
+
+void Audio::write(const uint8_t *data)
+{
+    int length = sizeof(data) / sizeof(data[0]);
+    uart_write_bytes(_port, data, length);
+}
+
+/*object:
+0-所有文件数量
+1-当前目录下文件数量
+2-当前文件名*/
+void Audio::query(uint8_t object)
+{
+    if (object == 0)
+        this->write(allfileNumQuery_cmd);
+    else if (object == 1)
+        this->write(fileNumQuery_cmd);
+    else if (object == 2)
+        this->write(fileNameQuery_cmd);
+}
+/*type:
+0-单曲播放
+1-随机播放
+2-循环播放*/
+void Audio::setPalyMode(uint8_t type)
+{
+    if (type == 0)
+        this->write(singlePlay_cmd);
+    else if (type == 1)
+        this->write(randomPlay_cmd);
+    else if (type == 2)
+        this->write(loopPlay_cmd);
+}
+// 路径播放
+void Audio::pathPlay(const char *path)
+{
+    int length = sizeof(path) / sizeof(path[0]);
+    uint8_t cmd[length + 5];
+
+    cmd[0] = 0xAA;
+    cmd[1] = 0x08;
+    cmd[2] = 0x04;
+    cmd[3] = 0x01;
+
+    for (size_t i = 0; i < length; i++)
+    {
+        cmd[i + 4] = path[i];
+    }
+
+    cmd[length + 4] = calculateADD8Check(cmd, length + 4);
+
+    this->write(cmd);
+}
+// 开始
+void Audio::start()
+{
+    this->write(startPlay_cmd);
+}
+// 停止
+void Audio::stop()
+{
+    this->write(stopPlay_cmd);
+}
+// 暂停
+void Audio::pause()
+{
+    this->write(pausePlay_cmd);
+}
+// 下一曲
+void Audio::next()
+{
+    this->write(nextPlay_cmd);
+}
+// 上一曲
+void Audio::previous()
+{
+    this->write(previous_cmd);
+}
+// 设定音量
+void Audio::setVolume(uint8_t volume)
+{
+    _volume = volume;
+}
+// 获取音量
+uint8_t Audio::getVolume()
+{
+    return _volume;
+}
+// 增加音量
+uint8_t Audio::improveVolume()
+{
+    if (_volume < 30)
+        _volume++;
+    return _volume;
+}
+// 减小音量
+uint8_t Audio::reduceVolume()
+{
+    if (_volume > 0)
+        _volume--;
+    return _volume;
 }
--- components/audio/audio.h
+++ components/audio/audio.h
@@ -10,29 +10,43 @@
 class Audio
 {
 private:
-    uint16_t _id;
+    uint8_t _volume;
     uart_port_t _port;
 
-    const uint8_t file_query[4] = {0xAA, 0x1E, 0x00, 0xC8};
-    const uint8_t number_query[4] = {0xAA, 0x12, 0x00, 0xBC};
-    const uint8_t allnum_query[4] = {0xAA, 0x0C, 0x00, 0xB6};
-    const uint8_t nextcomand[4] = {0xAA, 0x06, 0x00, 0xB0};
-    const uint8_t previouscomand[4] = {0xAA, 0x05, 0x00, 0xAF};
-    const uint8_t outputDeviceSDcardcomand[5] = {0xAA, 0x0B, 0x01, 0x01, 0xB7};
-    const uint8_t startcomand[4] = {0xAA, 0x02, 0x00, 0xAC};
-    const uint8_t pausecomand[4] = {0xAA, 0x03, 0x00, 0xAD};
-    const uint8_t stopcomand[4] = {0xAA, 0x04, 0x00, 0xAE};
-    const uint8_t randomAllcomand[5] = {0xAA, 0x18, 0x01, 0x05, 0xC8};
-    const uint8_t enableLoopcomand[5] = {0xAA, 0x18, 0x01, 0x04, 0xC7};
-    const uint8_t disableLoopcomand[5] = {0xAA, 0x18, 0x01, 0x02, 0xC5};
+    const uint8_t fileNameQuery_cmd[4] = {0xAA, 0x1E, 0x00, 0xC8};
+    const uint8_t fileNumQuery_cmd[4] = {0xAA, 0x12, 0x00, 0xBC};
+    const uint8_t allfileNumQuery_cmd[4] = {0xAA, 0x0C, 0x00, 0xB6};
+    const uint8_t volumeSet_cmd[5] = {0xAA, 0x13, 0x01, 0x1E, 0xDC};
+    const uint8_t nextPlay_cmd[4] = {0xAA, 0x06, 0x00, 0xB0};
+    const uint8_t previous_cmd[4] = {0xAA, 0x05, 0x00, 0xAF};
+    const uint8_t toSDcard_cmd[5] = {0xAA, 0x0B, 0x01, 0x01, 0xB7};
+    const uint8_t startPlay_cmd[4] = {0xAA, 0x02, 0x00, 0xAC};
+    const uint8_t pausePlay_cmd[4] = {0xAA, 0x03, 0x00, 0xAD};
+    const uint8_t stopPlay_cmd[4] = {0xAA, 0x04, 0x00, 0xAE};
+    const uint8_t randomPlay_cmd[5] = {0xAA, 0x18, 0x01, 0x05, 0xC8};
+    const uint8_t loopPlay_cmd[5] = {0xAA, 0x18, 0x01, 0x04, 0xC7};
+    const uint8_t singlePlay_cmd[5] = {0xAA, 0x18, 0x01, 0x02, 0xC5};
 
 public:
     Audio();
     Audio(uart_port_t port);
     ~Audio();
 
-    void write(const char *data);
-    uint16_t query_fileNumber();
+    void init();
+    void write(const uint8_t *data);
+    void query(uint8_t object);
+    void setPalyMode(uint8_t type);
+    void pathPlay(const char *path);
+    void start();
+    void stop();
+    void pause();
+    void next();
+    void previous();
+    void setVolume(uint8_t volume);
+
+    uint8_t getVolume();
+    uint8_t improveVolume();
+    uint8_t reduceVolume();
 };
 
 #endif
